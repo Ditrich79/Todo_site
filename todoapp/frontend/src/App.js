a6dd87ad69f8f433cd3b055e8d361b3dc2aa80cd
Ditrich79 <adk18@mail.ru>
2024-12-15 16:32:05 +0300

    last commit fullstack

--- todoapp/frontend/src/App.js
+++ todoapp/frontend/src/App.js
@@ -9,48 +9,72 @@ import Signup from './components/signup';
 
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Navbar';
 
+import TodoDataService from './services/todos';
+
 function App() {
-  // const user = null;
   const [user, setUser] = React.useState(null);
   const [token, setToken] = React.useState(null);
   const [error, setError] = React.useState('');
 
   async function login(user = null){ // default user to null
-    setUser(user);
+    TodoDataService.login(user)
+      .then(response =>{        
+        setToken(response.data.token);     
+        setUser(user.username);
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('user', user.username);
+        setError('');
+      })
+      .catch( e =>{
+        console.log('login', e);
+        setError(e.toString());       
+      });
   }
 
   async function logout(){
-    setUser(null);
+    setToken('');
+    setUser('');
+    localStorage.setItem('token', '');
+    localStorage.setItem('user', ''); 
   }
 
   async function signup(user = null){ // default user to null
-    setUser(user);
+    TodoDataService.signup(user)
+      .then(response =>{
+        setToken(response.data.token);
+        setUser(user.username);
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('user', user.username);
+      })
+      .catch( e =>{
+        console.log(e);
+        setError(e.toString());
+      })
   }
   
   return (
     <div className="App">
       <Navbar bg="primary" variant="dark">
-        <div class="container-fluid">
+        <div className="container-fluid">
           <Navbar.Brand>TodosApp</Navbar.Brand>
           <Nav className="me-auto">
             <Container>
-              <Link class="nav-link" to={"/todos"}>Todos</Link>
+              <Link className="nav-link" to={"/todos"}>Todos</Link>
               { user ? (
-                <Link class="nav-link">Logout ({user})</Link>
+                <Link className="nav-link" onClick={logout}>Logout ({user})</Link>
               ) : (
                 <>
-                  <Link class="nav-link" to={"/login"}>Login</Link>
-                  <Link class="nav-link" to={"/signup"}>Sign Up</Link>
+                  <Link className="nav-link" to={"/login"}>Login</Link>
+                  <Link className="nav-link" to={"/signup"}>Sign Up</Link>
                 </>
               )}
             </Container>
           </Nav>
         </div>
       </Navbar>
-
+      
       <div className="container mt-4">
         <Switch>	
           <Route exact path={["/", "/todos"]} render={(props) =>
@@ -75,7 +99,7 @@ function App() {
           </Route>
         </Switch>
       </div>
-
+      
       <footer className="text-center text-lg-start 
         bg-light text-muted mt-4">
         <div className="text-center p-4">
@@ -94,7 +118,6 @@ function App() {
           </a>
         </div>
       </footer>
-
     </div>
   );
 }
